da2fd6c8ced7f02bed724312462d3879429bf1a2
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-10-02 19:48:34 -0300

    add env

--- myApp.js
+++ myApp.js
@@ -1,13 +1,14 @@
 let express = require('express');
 let app = express();
-
+import dotenv from "dotenv";
+dotenv.config({path: "/.env" })
 app.use('/public', express.static(__dirname +'/public', ));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 });
 app.get("/json", (req, res) => {
-    res.json({message: "Hello json"})
+    res.json({message: process.env.MESSAGE_STYLE})
 })
 app.listen(3000)
 
