3c17cb4de312db4b155b473bddb67b7b356f7c63
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-10-02 19:38:32 -0300

    add middleware

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 let express = require('express');
 let app = express();
 
+app.use('/public', express.static(__dirname +'/public', ));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 });
