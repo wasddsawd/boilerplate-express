85f39626b03ca599ef2762934fc0c7aab47ce64c
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-10-02 19:43:16 -0300

    add rota json

--- myApp.js
+++ myApp.js
@@ -6,6 +6,9 @@ app.use('/public', express.static(__dirname +'/public', ));
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 });
+app.get("/json", (req, res) => {
+    res.json({message: "Hello json"})
+})
 app.listen(3000)
 
 
