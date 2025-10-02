f7f1c10101b3da2b4567ed432d0fc1a7f691cf77
wasddsawd <rafaelkrugervidaldasilva@gmail.com>
2025-10-02 19:33:34 -0300

    fix caminho

--- myApp.js
+++ myApp.js
@@ -2,8 +2,7 @@ let express = require('express');
 let app = express();
 
 app.get("/", (req, res) => {
-    const absolutePath = __dirname + 'views/index.html'
-    res.sendFile(absolutePath)
+    res.sendFile(__dirname + "/views/index.html")
 });
 app.listen(3000)
 
